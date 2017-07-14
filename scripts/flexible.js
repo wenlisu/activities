655979c3773d6d376ee522466120fec82d454065
laipp <646404645@qq.com>
2017-07-14 17:00:01 +0800

    fix styles bug

--- scripts/flexible.js
+++ scripts/flexible.js
@@ -5,7 +5,7 @@
             var clientWidth =docEl.clientWidth;
             if(!clientWidth) return;
             if(clientWidth > 414){
-                docEl.style.fontSize='50px';
+                docEl.style.fontSize='65px';
             }else{
                 docEl.style.fontSize=100 * (clientWidth / 640)+'px';//750这个值，根据设计师的psd宽度来修改，是多少就多少
             }
